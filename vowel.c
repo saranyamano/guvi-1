6f2315d339aff86451ea50d56624db5cb42bb8fd
saranyamano <31506060+saranyamano@users.noreply.github.com>
2017-09-05 19:52:33 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+{
+printf("Entered character is a vowel");
+}
+else
+{
+printf("Entered character is a consonant");
+}
+getch();
+}
