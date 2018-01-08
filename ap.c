a8d808dc0c9c5edf77cb94e746480ccbfb4b1491
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-01-08 16:46:40 +0530

    Create ap.c

---
+++ ap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,a,d,val,sum=0,i;
+printf("Enter the no of terms in AP");
+scanf("%d",&n);
+printf("Enter the first term and common difference in AP);
+scanf("%d %d",&a,&d);
+val=a;
+for(i=0;i<n;i++)
+{
+printf("%d",value);
+sum=sum+val;
+val=val+d;
+}
+}
