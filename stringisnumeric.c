11e2691b9885ed6adce9e086ab19b876686dd05a
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-01-21 10:22:50 +0530

    Create stringisnumeric.c

---
+++ stringisnumeric.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,flag=0;
+char=a[10];
+printf("enter the string:\n");
+scanf("%s",&s[i]);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||if(s[i]=='9');
+{
+flag=flag+1;
+}
+}
+if(flag==0);
+{
+printf("Given string contains no numbers\n");
+else
+printf("Given string contains numbers\n");
+}
+return o;
+}
