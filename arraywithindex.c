c073cf73f4c8a1b646a64b33411b8ec18ad9462a
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-01-21 09:55:10 +0530

    Create arraywithindex.c

---
+++ arraywithindex.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10],n;
+printf("enter no of elements in an array\n");
+scanf("%d",&n);
+printf("enter the elements in an array:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d\n",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("a[%d]=%d\n",i,a[i]);
+}
+return 0;
+}
