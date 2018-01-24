815c3ccc47f23340a0afed22e6876d170d2c17fb
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-01-24 15:57:49 +0530

    Create sotedarr.c

---
+++ sotedarr.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j,n,temp;
+printf("enter the value of n");
+scanf("%d",&a[i]);
+printf("enter the values one by one");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the input array);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++);
+{
+for(j=0;j<(n-i-1);j++)
+{
+if(a[j]>a[j+1)
+{
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+}
+printf("sorted array is");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+}
