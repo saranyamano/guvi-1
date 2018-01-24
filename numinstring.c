d0b9d257fcf84d4ef6d64fc7a1464428d3b95a77
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-01-24 16:15:34 +0530

    Create numinstring.c

---
+++ numinstring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char s[100]='saranya72';
+int i,c=0;
+for(i=0;s[i];i++)
+{
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+c++;
+{
+printf("The number of numeric characters :\n",c);
+}
+}
+}
