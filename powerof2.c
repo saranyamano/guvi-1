1351fa8db71ab854c9c49893d3f452e84522dd06
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-02 10:56:02 +0000

    Create powerof2.c

---
+++ powerof2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(0
+{
+int n,i,p=1;
+printf("enter the number\n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+p=p*2;
+if(p==n)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return o;
+}
+}
