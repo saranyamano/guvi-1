b8455ebad90f1206e711d65d5c020483ebec2bc3
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-02 11:21:04 +0000

    Update ap1.c

--- ap1.c
+++ ap1.c
@@ -6,13 +6,13 @@ printf("Enter the no of terms in AP");
 scanf("%d",&n);
 printf("Enter the first term and common difference in AP");
 scanf("%d %d",&a,&d);
-sum=(n(2a+(n-1)d))/2;
+sum=(n(2*a+(n-1)d))/2;
   tn=a+(n-1)d;
   for(i=a;i<tn;i++)
   {
     if(i!=tn)
     {
-      printf("%d%,i);
+      printf("%d,i);
              else
              printf("%d"=%d",i,sum);
                     }
