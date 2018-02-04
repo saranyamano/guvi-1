6aad5dff398333dd801b25f1b0c335acd3788e69
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-04 16:23:26 +0530

    Create range1to10.c

---
+++ range1to10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>=1&&n<=10)
+{
+printf("The number is in the range of 1 to 10");
+}
+else
+{
+printf("The number is not in the range of 1 to 10");
+}
+}
